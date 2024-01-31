a7bdd3652a0ed1c75048339ea249f1fa728ac321
Ryan Ma <ryanma9629@gmail.com>
2024-01-31 12:03:33 +0800

    lang sep

--- cdd_with_llm.py
+++ cdd_with_llm.py
@@ -15,7 +15,7 @@ from chromadb.config import Settings
 from apify_client import ApifyClient
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-# from langchain_community.embeddings import DashScopeEmbeddings, HuggingFaceEmbeddings
+from langchain_community.embeddings import DashScopeEmbeddings, HuggingFaceEmbeddings, QianfanEmbeddingsEndpoint
 from langchain_community.llms import QianfanLLMEndpoint, Tongyi
 from langchain_community.utilities.bing_search import BingSearchAPIWrapper
 from langchain_community.utilities.google_serper import GoogleSerperAPIWrapper
@@ -24,17 +24,22 @@ from langchain_core.documents.base import Document
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
-from langchain_openai import ChatOpenAI, AzureChatOpenAI, AzureOpenAIEmbeddings
-from langchain_google_genai import GoogleGenerativeAI
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings, AzureChatOpenAI, AzureOpenAIEmbeddings
 
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
 def web_search(company_name: str,
                search_suffix: Union[str, None] = None,
                search_engine: str = 'Bing',  # 'Bing', 'Google'
                num_results: int = 10,
-               lang: str = 'zh-CN',  # 'zh-CN', 'zh-HK', 'zh-TW', 'en-US'
+               lang: str = 'en-US',  # 'zh-CN', 'zh-HK', 'zh-TW', 'en-US'
                ):
-    logging.info(f'Getting URLs from {search_engine} search...')
+    logger.info(f'Getting URLs from {search_engine} search...')
 
     if search_engine == 'Google':
         if lang == 'zh-CN':
@@ -52,7 +57,7 @@ def web_search(company_name: str,
         langchain_se = BingSearchAPIWrapper(
             k=num_results, search_kwargs={'mkt': lang})
     else:
-        logging.error(
+        logger.error(
             f'Search engine {search_engine} is not supported.')
         return
 
@@ -75,12 +80,12 @@ def web_search(company_name: str,
     return search_results
 
 
-search_results = web_search('BridgeWater', lang='en-US')
+# search_results = web_search('Theranos', lang='zh-CN')
 
 
 def fetch_web_content(urls: List[str],
                       min_text_length: int = 100):
-    logging.info('Getting detailed web content from each URL...')
+    logger.info('Getting detailed web content from each URL...')
 
     apify_client = ApifyClient(os.getenv('APIFY_API_TOKEN'))
     actor_call = apify_client.actor('apify/website-content-crawler').call(
@@ -101,18 +106,20 @@ def fetch_web_content(urls: List[str],
     return records
 
 
-records = fetch_web_content([item['url'] for item in search_results])
+# records = fetch_web_content([item['url'] for item in search_results])
 
 
 def doc_store(records: List[Dict],
-              company_name: str):
+              company_name: str,
+              lang: str = 'en-US' # 'zh-CN', 'zh-HK', 'zh-TW', 'en-US'
+              ):
 
-    logging.info('Storing fetched documents into Chroma...')
+    logger.info('Storing fetched documents into Chroma...')
     persistent_client = chromadb.PersistentClient(
         path='./chroma',
         settings=Settings(anonymized_telemetry=False)
     )
-    collection_name = uuid.uuid3(uuid.NAMESPACE_DNS, company_name).hex
+    collection_name = uuid.uuid3(uuid.NAMESPACE_DNS, company_name + lang).hex
 
     collection = persistent_client.get_or_create_collection(
         collection_name,
@@ -128,18 +135,11 @@ def doc_store(records: List[Dict],
     return collection_name
 
 
-doc_store(records, company_name='BridgeWater')
+# doc_store(records, company_name='Theranos', lang='zh-CN')
 
-
-def qa_over_docs(
-        company_name: str,
-        query: Union[str, None] = None,
-        lang='zh-CN',  # 'zh-CN', 'zh-HK', 'zh-TW', 'en-US',
-        llm_provider: str = 'AzureOpenAI'  # 'Alibaba', 'Baidu', 'Google', 'OpenAI', 'AzureOpenAI'
-):
+def template_by_lang(lang: str):
     if lang == 'zh-CN':
-        if query is None:
-            query = '''这家公司有哪些负面新闻？总结不超过3条主要的，每条独立一行列出，并给出信息出处的URL。
+        query = '''这家公司有哪些负面新闻？总结不超过3条主要的，每条独立一行列出，并给出信息出处的URL。
 '''
         qa_template = '''利用下列信息回答后面的问题。如果你不知道答案就直接回答'不知道'，不要主观编造答案。
 最多使用5句话，回答尽量简洁。
@@ -150,9 +150,9 @@ def qa_over_docs(
 
 有价值的回答：
 '''
+        no_info = '没有足够的信息回答该问题'
     elif lang == 'zh-HK' or lang == 'zh-TW':
-        if query is None:
-            query = '''這家公司有哪些負面新聞？總結不超過3條主要的，每條獨立一行列出，並給出資訊出處的URL。
+        query = '''這家公司有哪些負面新聞？總結不超過3條主要的，每條獨立一行列出，並給出資訊出處的URL。
 '''
         qa_template = '''利用下列資訊回答後面的問題。如果你不知道答案就直接回答'不知道'，不要主觀編造答案。
 最多使用5句話，回答儘量簡潔。
@@ -163,9 +163,9 @@ def qa_over_docs(
 
 有價值的回答：
 '''
+        no_info = '沒有足夠的資訊回答該問題'
     else:
-        if query is None:
-            query = '''What is the negative news about this company? 
+        query = '''What is the negative news about this company? 
 Summarize no more than 3 major ones, list each on a separate line, and give the URL where the information came from.
 '''
         qa_template = '''Use the following pieces of context to answer the question at the end.
@@ -178,21 +178,35 @@ Question: {question}
 
 Helpful Answer:
 '''
+        no_info = 'No enough information to answer this.'
+
+    return {'query': query, 'qa_template': qa_template, 'no_info': no_info}
+
+
+def qa_over_docs(
+        company_name: str,
+        query: Union[str, None] = None,
+        lang: str ='en-US',  # 'zh-CN', 'zh-HK', 'zh-TW', 'en-US',
+        # 'Alibaba', 'Baidu', 'HuggingFace', 'OpenAI', 'AzureOpenAI'
+        embedding_provider: str = 'AzureOpenAI',
+        llm_provider: str = 'AzureOpenAI'  # 'Alibaba', 'Baidu', 'OpenAI', 'AzureOpenAI'
+):
+    qa_template = template_by_lang(lang)['qa_template']
+    no_info = template_by_lang(lang)['no_info']
+    if query is None:
+        query = template_by_lang(lang)['query']
 
     persistent_client = chromadb.PersistentClient(
         path='./chroma',
         settings=Settings(anonymized_telemetry=False)
     )
-    # huggingface_ef = embedding_functions.HuggingFaceEmbeddingFunction(
-    #     api_key=os.getenv('HUGGINGFACE_API_TOKEN'),
-    #     model_name='sentence-transformers/all-MiniLM-L6-v2'
-    # )
 
-    collection_name = uuid.uuid3(uuid.NAMESPACE_DNS, company_name).hex
+    collection_name = uuid.uuid3(uuid.NAMESPACE_DNS, company_name + lang).hex
     try:
         collection = persistent_client.get_collection(collection_name)
     except ValueError:
-        logging.error(f'Collection {collection_name} does not exist.')
+        logger.error(f'Collection {collection_name} does not exist.')
+        return
 
     chroma_docs = collection.get()
 
@@ -202,20 +216,35 @@ Helpful Answer:
             page_content=chroma_docs['documents'][i], metadata=chroma_docs['metadatas'][i])
         langchain_docs.append(langchain_doc)
 
-    logging.info('Splitting documents into small chunks...')
+    logger.info('Splitting documents into small chunks...')
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=100,
     )
     chunked_docs = splitter.split_documents(langchain_docs)
 
+    logger.info(f'Documents embedding with provider {embedding_provider}')
+    if embedding_provider == 'Alibaba':
+        embedding_fc = DashScopeEmbeddings(
+            dashscope_api_key=os.getenv('DASHSCOPE_API_KEY'))
+    elif embedding_provider == 'Baidu':
+        embedding_fc = QianfanEmbeddingsEndpoint()
+    elif embedding_provider == 'HuggingFace':
+        embedding_fc = HuggingFaceEmbeddings()
+    elif embedding_provider == 'OpenAI':
+        embedding_fc = OpenAIEmbeddings()
+    elif embedding_provider == 'AzureOpenAI':
+        embedding_fc = AzureOpenAIEmbeddings(
+            azure_deployment=os.getenv('AZURE_OPENAI_EMB_DEPLOY'))
+    else:
+        logger.error(
+            f'Embedding provider {embedding_provider} is not supported.')
+        return
+
     langchain_chroma = Chroma(
         collection_name='Ephemeral_Collection_for_QA',
         client_settings=Settings(anonymized_telemetry=False),
-        # embedding_function=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-        # embedding_function=DashScopeEmbeddings()
-        embedding_function=AzureOpenAIEmbeddings(
-            azure_deployment=os.getenv('AZURE_OPENAI_EMB_DEPLOY'))
+        embedding_function=embedding_fc
     )
 
     langchain_chroma.add_documents(chunked_docs)
@@ -225,20 +254,18 @@ Helpful Answer:
         search_kwargs={'k': 3, 'fetch_k': 5}
     )
 
-    logging.info(f'Documents QA using LLM provied by {llm_provider}...')
+    logger.info(f'Documents QA using LLM provied by {llm_provider}...')
     if llm_provider == 'Alibaba':
         chat_llm = Tongyi(model_name='qwen-max', temperature=0)
     elif llm_provider == 'Baidu':
         chat_llm = QianfanLLMEndpoint(model='ERNIE-Bot', temperature=0.01)
-    elif llm_provider == 'Google':
-        chat_llm = GoogleGenerativeAI(model="gemini-pro", google_api_key=os.getenv('GOOGLE_API_KEY'))
     elif llm_provider == 'OpenAI':
         chat_llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0)
     elif llm_provider == 'AzureOpenAI':
         chat_llm = AzureChatOpenAI(azure_deployment=os.getenv(
             'AZURE_OPENAI_LLM_DEPLOY'), temperature=0)
     else:
-        logging.error(f'LLM provider {llm_provider} is not supported.')
+        logger.error(f'LLM provider {llm_provider} is not supported.')
         return
 
     # rag_prompt = PromptTemplate.from_template(qa_template)
@@ -258,30 +285,31 @@ Helpful Answer:
     except ValueError:  # Occurs when there is no relevant information
         return {
             'query': query,
-            'answer': 'I don\'t know.'
+            'answer': no_info
         }
 
-qa = qa_over_docs('BridgeWater', lang='en-US', llm_provider='Google')
+
+# qa = qa_over_docs('Theranos', lang='zh-CN', llm_provider='AzureOpenAI')
 
 
 if __name__ == '__main__':
-    logging.basicConfig(
-        format='%(asctime)s - %(levelname)s: %(message)s', level=logging.INFO)
-
     # Proj settings
     # COMPANY_NAME = 'Rothenberg Ventures Management Company, LLC.'
     # COMPANY_NAME = '红岭创投'
-    COMPANY_NAME = '东方甄选'
+    # COMPANY_NAME = '东方甄选'
     # COMPANY_NAME = '恒大财富'
     # COMPANY_NAME = '鸿博股份'
-    # COMPANY_NAME = '平安银行'
+    COMPANY_NAME = '平安银行'
     # COMPANY_NAME = 'Theranos'
-    # COMPANY_NAME = 'Bridge Water'
+    # COMPANY_NAME = 'BridgeWater'
     # COMPANY_NAME = 'SAS Institute'
-    # COMPANY_NAME = 'Apple Inc.'
+    # COMPANY_NAME = 'Apple Inc.
+    
+    LANG = 'zh-CN'
 
-    search_results = web_search(COMPANY_NAME)
+    search_results = web_search(COMPANY_NAME, lang=LANG)
     records = fetch_web_content([item['url'] for item in search_results])
-    collection_name = doc_store(records, COMPANY_NAME)
-    qa = qa_over_docs(COMPANY_NAME)
-    pprint.pprint(qa, compact=True)
+    collection_name = doc_store(records, COMPANY_NAME, lang=LANG)
+    qa = qa_over_docs(COMPANY_NAME, lang=LANG)
+    if qa:
+        pprint.pprint(qa, compact=True)
