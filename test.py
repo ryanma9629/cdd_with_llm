22d7853439a21e0c5b920fac33b486b0986a688b
Ryan Ma <ryanma9629@gmail.com>
2024-03-03 22:58:31 +0800

    test

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 from flask import Flask, session
 from flask_cors import CORS
+from flask_session import Session
 
 app = Flask(__name__)
-CORS(app)
-app.secret_key = "123"
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://localhost:5000"}})
+app.config['SECRET_KEY'] =  "asdasdasdasda"
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.get("/set_foo")
 def set_foo():
@@ -15,4 +18,4 @@ def get_foo():
     return session["foo"]
 
 if __name__=="__main__":
-    app.run("localhost", 5000, debug=True)
+    app.run("localhost", 5000)
